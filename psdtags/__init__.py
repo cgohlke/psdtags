82f9388daeb8c86c874c3f9511c25e3662e70aed
Christoph Gohlke <cgohlke@cgohlke.com>
2025-02-22 10:12:08 -0800

    Update psdtags/__init__.py

--- psdtags/__init__.py
+++ psdtags/__init__.py
@@ -2,3 +2,16 @@
 
 from .psdtags import *
 from .psdtags import __all__, __doc__, __version__
+
+
+def _set_module() -> None:
+    """Set __module__ attribute for all public objects."""
+    globs = globals()
+    module = globs['__name__']
+    for item in __all__:
+        obj = globs[item]
+        if hasattr(obj, '__module__'):
+            obj.__module__ = module
+
+
+_set_module()
